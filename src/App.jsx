865970392b56de3e01444a690abe433bcb9bdfa2
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-12-15 02:24:57 +0100

    refactor: configure base project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { QueryClient, QueryClientProvider } from "react-query"
+import { Routes, Route } from "react-router-dom"
+import AuthProvider from "./auth/Auth"
+import AppContextProvider from "./context/AppContext"
+import Error from "./pages/Error.page"
+import Home from "./pages/Home.page"
+import Login from "./pages/Login.page"
+import ResetPassword from "./pages/ResetPassword.page"
+import Signup from "./pages/Signup.page"
+
+const queryClient = new QueryClient()
 function App() {
 
   return (
-    <div className="App">
-      
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <AppContextProvider>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/reset" element={<ResetPassword />} />
+              <Route path="*" element={<Error />} />
+            </Routes>
+          </div>
+        </AppContextProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   )
 }
 
