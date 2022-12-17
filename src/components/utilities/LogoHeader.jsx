91a611b44f7a193cc32d7fd6c9932df98ce593da
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-12-17 18:07:08 +0100

    refactor: remove unused code

--- src/components/utilities/LogoHeader.jsx
+++ src/components/utilities/LogoHeader.jsx
@@ -1,21 +1,9 @@
-import React, { useContext, useEffect, useRef } from 'react'
+import React from 'react'
 import Logo from '../../assets/media/Frame 7264.png'
-import { AppContext } from '../../context/AppContext'
-
 const LogoHeader = () => {
 
-  const {headerLogoHeight, setHeaderLogoHeight} = useContext(AppContext)
-
-  const height = useRef(0)
-
-  useEffect(() => {
-    setHeaderLogoHeight(height.current.offsetHeight)
-
-  }, [headerLogoHeight])
-  
-
   return (
-    <div ref={height} className=' flex items-center justify-center py-6'>
+    <div className=' flex items-center justify-center py-6'>
         <img className=' w-[144px]' src={Logo}/>
     </div>
   )
