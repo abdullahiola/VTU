f2cc23c9260d79745616322f2583dfafb0ff34d2
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-12-24 20:43:27 +0100

    feat(FE): made logo a link to home page

--- src/components/ui/LogoHeader.tsx
+++ src/components/ui/LogoHeader.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Logo from '../../assets/media/Frame 7264.svg'
 const LogoHeader = () => {
 
   return (
     <div className=' flex items-center justify-center py-6'>
+      <Link to='/' className='block'>
         <img className=' w-[144px]' src={Logo}/>
+      </Link>
     </div>
   )
 }
