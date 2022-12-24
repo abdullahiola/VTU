e18339872d780845cfcbb1c97485b6fb58fc9278
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-12-24 01:28:59 +0100

    feat(FE): maintain user authentication and validity

--- src/auth/Auth.tsx
+++ src/auth/Auth.tsx
@@ -1,18 +1,27 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 
 type AuthPropsType = {
   children: React.ReactNode
 }
 
 type AuthContextType = {
-  
+  isLoggedIn: boolean
+  setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>
+  login: () => void
+  logout: () => void
 }
 
-const AuthContext = createContext({} as AuthContextType)
+export const AuthContext = createContext({} as AuthContextType)
 const AuthProvider = ({children, ...props}: AuthPropsType) => {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const login = () => setIsLoggedIn(true)
+
+  const logout = () => setIsLoggedIn(false)
   
   return (
-    <AuthContext.Provider value={''}>
+    <AuthContext.Provider value={{isLoggedIn, setIsLoggedIn, login, logout}}>
       {children}
     </AuthContext.Provider>
   )
