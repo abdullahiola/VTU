2aef3208c9502877947e11c9d84bbb03c43fb785
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-12-26 02:31:29 +0100

    code(FE): add prop type for Tab component in home page

--- src/types/dashboard.types.tsx
+++ src/types/dashboard.types.tsx
@@ -1,3 +1,9 @@
 export type DashboardType = {
   Component: React.ComponentType
 }
+
+export type HomeTabsPropsType = {
+  path: string
+  urlPath: string
+  setPath: React.Dispatch<React.SetStateAction<string>>
+}
