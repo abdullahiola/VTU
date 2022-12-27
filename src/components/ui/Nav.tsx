abba843b65855a295c83336f8dc18981599cc86b
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-12-27 08:31:23 +0100

    fix(FE): fix responsiveness issues

--- src/components/ui/Nav.tsx
+++ src/components/ui/Nav.tsx
@@ -21,15 +21,15 @@ const Nav = () => {
            />
         </div>
       </div>
-      <div className='hidden py-[30px] px-12 items-center justify-between md:flex'>
+      <div className='hidden py-[30px] px-12 gap-x-6 items-center justify-between md:flex'>
         <div className=' w-[144px]'><Link to='/' className='w-full h-full'><img src={Logo} className='w-full h-full' /></Link></div>
-        <nav>
-          <ul className='flex items-center justify-between gap-x-12'>
-            <li><Link className='nav__link text-base font-semibold capitalize px-6 py-3 hover:text-green-800' to='/'>Home</Link></li>
-            <li><Link className='nav__link text-base font-semibold capitalize px-6 py-3 hover:text-green-800' to='/wallet'>Wallet</Link></li>
-            <li><Link className='nav__link text-base font-semibold capitalize px-6 py-3 hover:text-green-800' to='/transactions'>Transactions</Link></li>
-            <li><Link className='nav__link text-base font-semibold capitalize px-6 py-3 hover:text-green-800' to='/beneficiaries'>Beneficiaries</Link></li>
-            <li><Link className='nav__link text-base font-semibold capitalize px-6 py-3 hover:text-green-800' to='/analytics'>Analytics</Link></li>
+        <nav className='w-2/3'>
+          <ul className='flex items-center justify-between'>
+            <li><Link className='nav__link text-base font-semibold capitalize py-3 hover:text-green-800' to='/'>Home</Link></li>
+            <li><Link className='nav__link text-base font-semibold capitalize py-3 hover:text-green-800' to='/wallet'>Wallet</Link></li>
+            <li><Link className='nav__link text-base font-semibold capitalize py-3 hover:text-green-800' to='/transactions'>Transactions</Link></li>
+            <li><Link className='nav__link text-base font-semibold capitalize py-3 hover:text-green-800' to='/beneficiaries'>Beneficiaries</Link></li>
+            <li><Link className='nav__link text-base font-semibold capitalize py-3 hover:text-green-800' to='/analytics'>Analytics</Link></li>
           </ul>
         </nav>
         <div>
