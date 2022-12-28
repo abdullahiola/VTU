cf22d5abcb7f53fd8b658831e7dfc4d1c7f9195f
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-12-28 15:49:58 +0100

    fix(FE): add padding to parent container; fix(FE): import correct transactions component

--- src/components/ui/BuyAirtime.tsx
+++ src/components/ui/BuyAirtime.tsx
@@ -1,15 +1,15 @@
 import Icon1 from '../../assets/media/card-icon1.svg'
 import Icon2 from '../../assets/media/card-icon2.svg'
 import Icon3 from '../../assets/media/card-icon3.svg'
-import Transaction from '../dashboard/Transaction'
 import Card from './Card'
 import Favorites from './Favorites'
+import Transactions from './Transactions'
 
 const BuyAirtime = () => {
   const msg = 'All networks in Nigeria'
   return (
     <div>
-      <section className='overflow-x-scroll pt-4 pb-16 md:pb-20'>
+      <section className='overflow-x-scroll pt-4 pb-16 px-6 md:px-12 md:pb-20'>
         <div className='flex items-center w-max justify-between gap-x-4 px-1 md:w-full md:gap-x-8'>
           <Card bgColor='#C9DDF7' icon={Icon1} title='Mtn' detail={msg} />
           <Card bgColor='#F4E8D3' icon={Icon2} title='Glo' detail={msg} />
@@ -18,7 +18,7 @@ const BuyAirtime = () => {
         </div>
       </section>
       <Favorites />
-      <Transaction />
+      <Transactions />
     </div>
   )
 }
