e8452d452a214198c1c6fee2a9d4ebf5adf4ed36
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-01-04 21:05:10 +0100

    feat(FE): add loading state for fullscreen mode

--- src/components/utilities/Loading.tsx
+++ src/components/utilities/Loading.tsx
@@ -6,30 +6,38 @@ import Overlay from './Overlay'
 type FullType = {
   full: boolean
   mini?: never
+  screen?: never
 }
 
 type MiniType = {
   mini: boolean
   full?: never
+  screen?: never
 }
 
-type LoadingPropsType = FullType | MiniType
+type ScreenType = {
+  screen: boolean
+  mini?: never
+  full?: never
+}
 
-const Loading = ({full, mini, ...props}: LoadingPropsType) => {
+type LoadingPropsType = FullType | MiniType | ScreenType
+
+const Loading = ({full, mini, screen, ...props}: LoadingPropsType) => {
 
   const elem = document.querySelector(".app") as HTMLDivElement
 
   const genSpinnerButtons = () => { 
     return (
       <>
-        <li className={` ${mini ? "w-2" : "w-8"} ${mini ? "h-2" : "h-8"} ${full && "animate-[loading_ease_1s_infinite_0s] top-[13%] right-[12%]"} rounded-full bg-white absolute top-[13%] right-[12%]`}></li>
-        <li className={` ${mini ? "w-2" : "w-8"} ${mini ? "h-2" : "h-8"} ${full && "animate-[loading_ease_1s_infinite_0.1s] top-[calc(50%-1rem)] right-0 -translate-y-0"} rounded-full bg-white absolute top-1/2 right-0 -translate-y-1/2`}></li>
-        <li className={` ${mini ? "w-2" : "w-8"} ${mini ? "h-2" : "h-8"} ${full && "animate-[loading_ease_1s_infinite_0.2s]"} rounded-full bg-white absolute bottom-[13%] right-[12%]`}></li>
-        <li className={` ${mini ? "w-2" : "w-8"} ${mini ? "h-2" : "h-8"} ${full && "animate-[loading_ease_1s_infinite_0.3s] bottom-0 left-[calc(50%-1rem)] -translate-x-0"} rounded-full bg-white absolute bottom-0 left-1/2 -translate-x-1/2`}></li>
-        <li className={` ${mini ? "w-2" : "w-8"} ${mini ? "h-2" : "h-8"} ${full && "animate-[loading_ease_1s_infinite_0.4s]"} rounded-full bg-white absolute bottom-[13%] left-[12%]`}></li>
-        <li className={` ${mini ? "w-2" : "w-8"} ${mini ? "h-2" : "h-8"} ${full && "animate-[loading_ease_1s_infinite_0.5s] top-[calc(50%-1rem)] left-0  -translate-y-0"} rounded-full bg-white absolute top-1/2 left-0  -translate-y-1/2`}></li>       
-        <li className={` ${mini ? "w-2" : "w-8"} ${mini ? "h-2" : "h-8"} ${full && "animate-[loading_ease_1s_infinite_0.6s]"} rounded-full bg-white absolute top-[13%] left-[12%]`}></li>
-        <li className={` ${mini ? "w-2" : "w-8"} ${mini ? "h-2" : "h-8"} ${full && "animate-[loading_ease_1s_infinite_0.7s] top-0 left-[calc(50%-1rem)] -translate-x-0"} rounded-full bg-white absolute top-0 left-1/2 -translate-x-1/2`}></li>
+        <li className={` ${mini ? "w-2" : "w-8"} ${mini ? "h-2" : "h-8"} ${(full || screen) && "animate-[loading_ease_1s_infinite_0s] top-[13%] right-[12%]"} rounded-full bg-white absolute top-[13%] right-[12%]`}></li>
+        <li className={` ${mini ? "w-2" : "w-8"} ${mini ? "h-2" : "h-8"} ${(full || screen) && "animate-[loading_ease_1s_infinite_0.1s] top-[calc(50%-1rem)] right-0 -translate-y-0"} rounded-full bg-white absolute top-1/2 right-0 -translate-y-1/2`}></li>
+        <li className={` ${mini ? "w-2" : "w-8"} ${mini ? "h-2" : "h-8"} ${(full || screen) && "animate-[loading_ease_1s_infinite_0.2s]"} rounded-full bg-white absolute bottom-[13%] right-[12%]`}></li>
+        <li className={` ${mini ? "w-2" : "w-8"} ${mini ? "h-2" : "h-8"} ${(full || screen) && "animate-[loading_ease_1s_infinite_0.3s] bottom-0 left-[calc(50%-1rem)] -translate-x-0"} rounded-full bg-white absolute bottom-0 left-1/2 -translate-x-1/2`}></li>
+        <li className={` ${mini ? "w-2" : "w-8"} ${mini ? "h-2" : "h-8"} ${(full || screen) && "animate-[loading_ease_1s_infinite_0.4s]"} rounded-full bg-white absolute bottom-[13%] left-[12%]`}></li>
+        <li className={` ${mini ? "w-2" : "w-8"} ${mini ? "h-2" : "h-8"} ${(full || screen) && "animate-[loading_ease_1s_infinite_0.5s] top-[calc(50%-1rem)] left-0  -translate-y-0"} rounded-full bg-white absolute top-1/2 left-0  -translate-y-1/2`}></li>       
+        <li className={` ${mini ? "w-2" : "w-8"} ${mini ? "h-2" : "h-8"} ${(full || screen) && "animate-[loading_ease_1s_infinite_0.6s]"} rounded-full bg-white absolute top-[13%] left-[12%]`}></li>
+        <li className={` ${mini ? "w-2" : "w-8"} ${mini ? "h-2" : "h-8"} ${(full || screen) && "animate-[loading_ease_1s_infinite_0.7s] top-0 left-[calc(50%-1rem)] -translate-x-0"} rounded-full bg-white absolute top-0 left-1/2 -translate-x-1/2`}></li>
       </>
     )
    }
@@ -52,6 +60,17 @@ const Loading = ({full, mini, ...props}: LoadingPropsType) => {
       </ul>
     )
    }
+   
+   else if (screen) {
+    return (
+      <Overlay opacity={0.7}>
+         <ul className=" bg-transparent w-[180px] h-[180px] relative rounded-[50%]">
+          {genSpinnerButtons()}
+         </ul>
+       </Overlay>
+    )
+   }
+   
 
    else {
     return (
